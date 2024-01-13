44117abe0f95fcdc6533b53afcfbc0bfd7536857
Lizz-Reid <139030289+Lizz-Reid@users.noreply.github.com>
2024-01-13 18:28:00 +1100

    further update

---
+++ index.js
@@ -0,0 +1,8 @@
+$('.card').function myFunction() {
+  var x = document.getElementById("card1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
